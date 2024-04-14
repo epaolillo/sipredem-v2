f28233b3c04ca3ed45cdf225fa08de0403ff8645
Ezequiel Paolillo <ezequiel.paolillo@univtec.com>
2024-04-14 18:11:27 -0300

    Reduced to 100k

--- replicate.js
+++ replicate.js
@@ -3,7 +3,7 @@ const iconv = require('iconv-lite');
 const { createClient } = require('@clickhouse/client');
 
 const OFFSET = 0;
-const LIMIT = 1_000_000;
+const LIMIT = 100_000;
 
 const clickhouse = createClient({
   database: 'padron',
