2781eeb9646cf4dc898a6d70d39e891efb06064d
Ezequiel Paolillo <ezequiel.paolillo@univtec.com>
2024-04-14 17:37:15 -0300

    Added dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+require('dotenv').config();
 
 const { createClient } = require('@clickhouse/client');
 
